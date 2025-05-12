5e19ae115f4765baf7feb1928340f22148312fb4
lital <litallital@gmail.com>
2025-05-12 16:16:25 +0300

    style

--- main.js
+++ main.js
@@ -7,7 +7,17 @@ const exerciseFiles = [
   './exercise1.js',
   './exercise2.js',
   './exercise3.js',
-  './exercise4.js'
+  './exercise4.js',
+  './exercise5.js',
+  './exercise6.js',
+  './exercise7.js', 
+  './exercise8.js',
+  './exercise9.js',
+  './exercise10.js',
+  './exercise11.js',
+  './exercise12.js',
+  './exercise13.js',
+  './exercise14.js',
   // Add more as needed
 ];
 
