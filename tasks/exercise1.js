457f14bc24c286003e187f8322807b81f5502cd9
Lee Ballan <leeballan@gmail.com>
2025-05-14 15:26:35 +0300

    Ex1: reorder images

--- tasks/exercise1.js
+++ tasks/exercise1.js
@@ -27,10 +27,10 @@ class Exercise1 extends Exercise {
   composeImageHtml(vars) {
     const { color, clouds, rainbow } = vars;
     const colorKey = Object.keys(hebrewDict.colors).find(key => hebrewDict.colors[key] === color);
+    const rainbowImg = rainbow === hebrewDict.yes ? 'ex1/rainbow.png' : null;
     const forestImg = `ex1/forest_${colorKey}.png`;
     const cloudsImg = clouds !== '0' ? `ex1/clouds_${clouds}.png` : null;
-    const rainbowImg = rainbow === hebrewDict.yes ? 'ex1/rainbow.png' : null;
-    return this.generateImageHTML([forestImg, cloudsImg, rainbowImg]);
+    return this.generateImageHTML([rainbowImg, forestImg, cloudsImg]);
   }
 
   getDefaultHtml() {
