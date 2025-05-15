e25b32c89a76ff8c5c25581b4e73373bdc9e0eb8
Lital <litallital@gmail.com>
2025-05-15 10:12:10 +0300

    fix ex2 and ex8

--- tasks/exercise2.js
+++ tasks/exercise2.js
@@ -6,20 +6,28 @@ class Exercise2 extends Exercise {
     super(hebrewDict.ex2.title);
   }
   levelFieldTypes = {'easy': 'input', 'hard': 'input'};
-  validColors = ['כחול', 'ירוק', 'ורוד', , 'סגול', 'אדום', 'צהוב'];
+  validColors = [
+    hebrewDict.colors.blue,
+    hebrewDict.colors.green,
+    hebrewDict.colors.pink,
+    hebrewDict.colors.purple,
+    hebrewDict.colors.red,
+    hebrewDict.colors.yellow,
+  ]
   validSpots = ['0', '1', '2', '3', '4', '5'];
-  defaultSpots = 1;
-  defaultColor = "אדום";
+
+  inputColor = '';
+  inputSpots = '';
 
   getCodeParts() {
     let color_field, spots_field;
     if (this.level === 'easy') {
-      color_field = this.createFieldDisplayDetails({pretext: `${hebrewDict.ex2.mushrooms_color} = `, default_value: this.defaultColor});
-      spots_field = this.createFieldDisplayDetails({pretext: `${hebrewDict.ex2.spots} = `, default_value: this.defaultSpots}); 
+      color_field = this.createFieldDisplayDetails({pretext: `${hebrewDict.ex2.mushrooms_color} = `});
+      spots_field = this.createFieldDisplayDetails({pretext: `${hebrewDict.ex2.spots} = `}); 
     }
     else {
-      color_field = this.createFieldDisplayDetails({default_value: this.defaultColor});
-      spots_field = this.createFieldDisplayDetails({default_value: this.defaultSpots});
+      color_field = this.createFieldDisplayDetails({});
+      spots_field = this.createFieldDisplayDetails({});
     }
     const combined = color_field.concat(spots_field);
 
@@ -43,36 +51,47 @@ class Exercise2 extends Exercise {
     return this.generateImageHTML([backgroundImg, mushroomImg]);
   }
 
-  extractInputs(selects, inputs, only_values=false) {
-    let color_string, spots_string, color_var, spots_var, color, spots;
+  extractInputs(inputs) {
+    let input1, input2, val1, val2, varName1, varName2;
     if (this.level === 'easy') {
-      [color, spots] = Array.from(inputs).map(s => s.value.trim());
+      [val1, val2] = Array.from(inputs).map(s => s.value.trim());
+      varName1 = hebrewDict.ex2.mushrooms_color;
+      varName2 = hebrewDict.ex2.spots;      
     }
     else {
-      [color_string, spots_string] = Array.from(inputs).map(i => i.value.trim());
-      const color_parts = color_string.split('=');
-      if (color_parts.length < 2) return '';
-      color_var = color_parts[0].trim();
-      color = color_parts[1].trim();
-
-      const spots_parts = spots_string.split('=');
-      if (spots_parts.length < 2) return '';
-      spots_var = spots_parts[0].trim();
-      spots = spots_parts[1].trim();
+      [input1, input2] = Array.from(inputs).map(i => i.value.trim());
+      [varName1, val1]  = this.extractSingleInput(input1);
+      [varName2, val2]  = this.extractSingleInput(input2);
     }
-    if (only_values) return { color, spots };
-    return {color_var, color, spots_var, spots};
+    return {varName1, val1, varName2, val2};
   }
 
-  handleRun({ selects, inputs }) {
-    const {color, spots} = this.extractInputs(selects, inputs, true);
-    return this.composeImageHtml({color, spots});
+  extractSingleInput(input) {
+    const regex = /^\s*([\u0590-\u05FF]+)\s*=\s*([\u0590-\u05FF0-9]+)\s*$/;
+      
+      const match = input.match(regex);
+      if (!match) {
+        return null;
+      }
+      const varName = match[1].trim();
+      const val = match[2].trim();
+      return [ varName, val ];
   }
 
-  isCorrect(color, spots) {
-    return (
-      color === "אדום" && spots === "4"
-    );
+  handleRun() {
+    return this.composeImageHtml({
+      color: this.inputColor, 
+      spots: this.inputSpots
+    });
+  }
+
+
+  isCorrect() {
+    if (this.inputColor === hebrewDict.colors.red && this.inputSpots === "4"){
+      return { valid: true, message: hebrewDict.ex2.success };
+    } else {
+      return { valid: false, message: hebrewDict.ex2.wrong_answer };
+    }
   }
 
   isValid(color, spots) {
@@ -82,33 +101,59 @@ class Exercise2 extends Exercise {
     );
   }
 
-  validate({ selects, inputs }) {
+  validate({ inputs }) {
     let color, spots;
-    if (this.level === 'easy') {
-      const {color, spots} = this.extractInputs(selects, inputs);
-      if (!this.isValid(color, spots)) {
-        return { valid: false, message: this.getErrorMessage() };
-      }
-    }
-    else {
-      const {color_var, color, spots_var, spots} = this.extractInputs(selects, inputs);
-      if (!this.isValid(color, spots)) {
-        return { valid: false, message: this.getErrorMessage() };
-      }
-      if (color_var !== 'צבע' || spots_var !== 'נקודות') {
-        return { valid: false, message: hebrewDict.ex2.fields_error_message };
-      }
-    }
+    const {varName1, val1, varName2, val2} = this.extractInputs(inputs);
+     if (!varName1 || !val1 || !varName2 || !val2) {
+        return { valid: false, message: hebrewDict.ex2.error_message };
+     }
 
-    return { valid: true, message: this.getValidMessage()};
+      // analyze the first input
+      if (varName1 === hebrewDict.ex2.mushrooms_color) {
+        color = val1;
+      } else if (varName1 === hebrewDict.ex2.spots) {
+        spots = val1;
+      } else {
+        return { valid: false, message: hebrewDict.ex2.failure_var_doesnt_exist };
+      }
+      // analyze the second input
+     if (varName2 === hebrewDict.ex2.mushrooms_color) {
+        color = val2;
+      } else if (varName2 === hebrewDict.ex2.spots) {
+        spots = val2;
+      } else {
+        return { valid: false, message: hebrewDict.ex2.failure_var_doesnt_exist };
+      }
+
+      if (varName1 === varName2) {
+        return { valid: false, message: hebrewDict.ex2.failure_var_is_missing };
+      }
+    
+    // check if the values are valid
+     if (!this.isValid(color, spots)) {
+        return { valid: false, message: this.getErrorMessage(color, spots) };
+    }    
+
+    // set the values
+    this.inputColor = color;
+    this.inputSpots = spots;
+
+    return { valid: true, message: ""};
   }
 
   getValidMessage() {
     return hebrewDict.ex2.success;
   }
 
-  getErrorMessage() {
-    return hebrewDict.ex2.error_message;
+  getErrorMessage(color, spots) {
+    let message = [];
+    if (!this.validColors.includes(color)) {
+      message.push(hebrewDict.ex2.color_error);
+    }
+    if (!this.validSpots.includes(spots)) {
+      message.push(hebrewDict.ex2.spots_error);
+    }
+    return message.join('<br>');
   }
 }
 
